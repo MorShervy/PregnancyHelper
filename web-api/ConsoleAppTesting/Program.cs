8c73c214e9b25aba2d78c55e91b9f94071756212
MorShervy <morshervy@gmail.com>
2019-09-01 12:06:26 +0300

    init web-api

---
+++ web-api/ConsoleAppTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
