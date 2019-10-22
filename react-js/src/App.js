e7ba5916f22d9a777d26c6fbe1cdab791e207d42
MorShervy <morshervy@gmail.com>
2019-10-22 16:35:00 +0300

    react js

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -3,6 +3,8 @@ import { withRouter, HashRouter, Route } from "react-router-dom";
 import './App.css';
 import SendEmailResetPassword from './pages/sendEmailResetPassword/SendEmailResetPassword';
 import ResetPassword from './pages/resetPassword/ResetPassword';
+import EmailSent from './pages/sendEmailResetPassword/EmailSent';
+import PasswordUpdated from './pages/resetPassword/PasswordUpdated';
 
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
     <HashRouter>
       <div>
         <Route exact path="/sendemail" component={SendEmailResetPassword} />
+        <Route exact path="/emailsent" component={EmailSent} />
         <Route exact path="/resetpassword/:uid" component={ResetPassword} />
+        <Route exact path="/passwordupdated" component={PasswordUpdated} />
       </div>
     </HashRouter>
   );
